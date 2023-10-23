6f32065565866751907dbb266dd5fdb13b8e272e
GhostgamesDHG <dgcooldjd@outlook.com>
2023-10-23 23:05:33 +0200

    Initial commit

---
+++ copy.bat
@@ -0,0 +1,2 @@
+@echo off
+move "C:\MinecraftModding\MinetopiaModFolder\MinetopiaMod\build\libs\MineTopiaMod-2.0.jar" "C:\Users\DGcoo\Desktop"
---
+++ src/main/java/com/armedendmion/minetopiamod/MineTopiaMod.java
@@ -0,0 +1,50 @@
+package com.armedendmion.minetopiamod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+@Mod(MineTopiaMod.MOD_ID)
+public class MineTopiaMod
+{
+    public static final String MOD_ID = "gmm";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MineTopiaMod()
+    {
+        IEventBus Bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        Bus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
